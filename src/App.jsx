2920ae2c2490234d680ffd6384afda067497b1c0
Hsiangting <stpeng777@gmail.com>
2025-12-15 13:24:41 -0800

    adds useState hook to manage messages state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import messages from './data/messages.json';
 import ChatLog from './components/ChatLog.jsx';
+import { useState } from 'react';
 // import ChatEntry from './components/ChatEntry.jsx';
 
 
 function App () {
+  const [messages, setMessages] = useState(messages);
   return (
     <div className="App">
       <ChatLog
